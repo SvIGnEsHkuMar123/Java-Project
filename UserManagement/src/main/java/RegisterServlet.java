9f8581dc654f0883a0227c6f016c0611cd40dabc
VIGNESH KUMAR S <113874733+SvIGnEsHkuMar123@users.noreply.github.com>
2024-04-29 13:28:02 +0530

    Add files via upload

---
+++ UserManagement/src/main/java/RegisterServlet.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    private final static String query = "insert into user(name,email,mobile,dob,city,gender) values(?,?,?,?,?,?)";
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        //get PrintWriter
+        PrintWriter pw = res.getWriter();
+        //set content type
+        res.setContentType("text/html");
+        //link the bootstrap
+        pw.println("<link rel='stylesheet' href='css/bootstrap.css'></link>");
+        //get the values
+        String name = req.getParameter("userName");
+        String email = req.getParameter("email");
+        String mobile = req.getParameter("mobile");
+        String dob = req.getParameter("dob");
+        String city = req.getParameter("city");
+        String gender = req.getParameter("gender");
+        //load the JDBC driver
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        //generate the connection
+        try(Connection con = DriverManager.getConnection("jdbc:mysql:///usermanagement","root","2002");
+                PreparedStatement ps = con.prepareStatement(query);){
+            //set the values
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, mobile);
+            ps.setString(4, dob);
+            ps.setString(5, city);
+            ps.setString(6, gender);
+            //execute the query
+            int count = ps.executeUpdate();
+            pw.println("<div class='card' style='margin:auto;width:300px;margin-top:100px'>");
+            if(count==1) {
+                pw.println("<h2 class='bg-secondary text-light text-center'>Record Registered Successfully</h2>");
+                pw.println("<h2 class= bg- info text-light text-center'><center>Thanks for Registering</center></h2>");
+            }else {
+                pw.println("<h2 class='bg-danger text-light text-center'>Record Not Registered</h2>");
+            }
+        }catch(SQLException se) {
+            pw.println("<h2 class='bg-danger text-light text-center'>"+se.getMessage()+"</h2>");
+            pw.println("<h2 class='bg-info text-light text-center'>"+se.getMessage()+"</h2>");
+            se.printStackTrace();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+        pw.println("<center><a href='Home.html'><button class='btn btn-outline-success diplay-block'>Home</button></a></center>");
+        pw.println("</div>");
+        //close the stream
+        pw.close();
+    }
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        doGet(req,res);
+    }
+}
